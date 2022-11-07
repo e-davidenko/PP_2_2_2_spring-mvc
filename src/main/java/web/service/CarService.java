c5127088aef1c8b41987def33cf205ff77cfc63b
Excecc <e.davidenko@live.ru>
2022-11-07 11:25:04 +0300

    UPDATED

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -2,28 +2,15 @@ package web.service;
 
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class CarService {
-    private List<Car> cars;
+/**
+ * Created in Intellij
+ * User: Davidenko
+ * Date: 07.11.2022
+ * Time: 11:02
+ */
 
-    {
-        cars = new ArrayList<>();
-        cars.add(new Car("BMW", 100, "Blue"));
-        cars.add(new Car("Mercedes", 200, "Black"));
-        cars.add(new Car("Lada", 2108, "Green"));
-        cars.add(new Car("Datsun", 2020, "White"));
-        cars.add(new Car("Volvo", 2222, "Yellow"));
-    }
-
-    public List<Car> getCars(int count) {
-        if (count >= 5) {
-            return cars;
-        } else if (count >= 0) {
-            return cars.stream().limit(count).toList();
-        } else {
-            return null;
-        }
-    }
+public interface CarService {
+    List<Car> getCars(int count);
 }
